5c898456f031db7f5dbfa7759c06975f42631cf1
bonetou <h.boneto@outlook.com>
2020-10-07 17:43:26 -0300

    homepage upgrade - home & aboutme

--- flutter_project/lib/components/homepage_default_controller.dart
+++ flutter_project/lib/components/homepage_default_controller.dart
@@ -3,13 +3,11 @@ import 'package:flutter_project/components/homepage_app_bar.dart';
 import 'package:flutter_project/pages/about.dart';
 import 'package:flutter_project/pages/contact.dart';
 import 'package:flutter_project/pages/home.dart';
-import 'package:flutter_project/pages/projects.dart';
 
 class HomepageDefaultController extends StatelessWidget {
   final List<String> myTabNames = [
     'INÍCIO',
     'SOBRE MIM',
-    'PROJETOS',
     'CONTATO'
   ];
   final String myTitle = 'BEATRIZ SEVERO';
@@ -23,7 +21,7 @@ class HomepageDefaultController extends StatelessWidget {
             preferredSize: Size.fromHeight(100),
             child: HomepageAppBar(title: myTitle, tabNames: myTabNames)),
         body: TabBarView(
-          children: <Widget>[Home(), About(), Projects(), Contact()],
+          children: <Widget>[Home(), About(), Contact()],
         ),
       ),
     );
